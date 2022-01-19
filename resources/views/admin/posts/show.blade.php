2c863b909e04ee9997d1d40a94f5e570aa9d40c0
alfredochs <chanta.alfredo@gmail.com>
2022-01-19 22:57:06 +0100

    destroy done

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -10,6 +10,12 @@
                 <h5 class="card-title">{{ $post->title }}</h5>
                 <p class="card-text">{{ $post->subtitle }}</p>
                 <a href="{{ route('admin.posts.edit', $post->id) }}" class="btn btn-primary">Modifica</a>
+
+                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" value="Elimina" class="btn btn-danger">
+                </form>
             </div>
         </div>
 
